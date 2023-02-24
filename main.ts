37d423de6c23f001f848ecddc858e563162a5986
Tin Ly <trungtin.sotech@gmail.com>
2023-02-24 15:30:32 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import {
     GetProductsForIngredient,
     GetRecipes
 } from "./supporting-files/data-access";
-import {NutrientFact, Product} from "./supporting-files/models";
+import {NutrientFact} from "./supporting-files/models";
 import {
     GetCostPerBaseUnit,
     GetNutrientFactInBaseUnits
@@ -19,58 +19,59 @@ const recipeSummary: any = {}; // the final result to pass into the test functio
  * YOUR CODE GOES BELOW THIS, DO NOT MODIFY ABOVE
  * (You can add more imports if needed)
  * */
+
 for (const recipe of recipeData) {
-  let cheapestCost = Infinity;
-  const nutrientsAtCheapestCost: Record<string, any> = {};
-
-  for (const lineItem of recipe.lineItems) {
-    const products = GetProductsForIngredient(lineItem.ingredient);
-    if (!products.length) {
-      console.log(`No products found for ingredient: ${lineItem.ingredient.ingredientName}`);
-      continue;
-    }
-
-    let nutrientFactInBaseUnits: NutrientFact | null = null;
-    let nutrientUnitOfMeasure: string | null = null;
-
-
-    for (const product of products) {
-      for (const supplierProduct of product.supplierProducts) {
-        const costPerBaseUnit = GetCostPerBaseUnit(supplierProduct);
-        if (costPerBaseUnit === null) {
-          console.log(`No cost per base unit found for product: ${product.productName}`);
-          continue;
-        }
-
-        cheapestCost = Math.min(cheapestCost, costPerBaseUnit);
-      }
-
-      const nutrientFact = product.nutrientFacts
-        .map(nf => GetNutrientFactInBaseUnits(nf))
-        .find(nf => nf !== null);
-      if (nutrientFact === null) {
-        console.log(`No nutrient information found for product: ${product.productName}`);
+    let cheapestCost = Infinity;
+    const nutrientsAtCheapestCost: Record<string, any> = {};
+  
+    for (const lineItem of recipe.lineItems) {
+      const products = GetProductsForIngredient(lineItem.ingredient);
+      if (!products.length) {
+        console.log(`No products found for ingredient: ${lineItem.ingredient.ingredientName}`);
         continue;
       }
-
-      nutrientFactInBaseUnits = nutrientFact;
-      nutrientUnitOfMeasure = lineItem.unitOfMeasure;
-      const nutrientAmount = nutrientFactInBaseUnits?.[nutrientUnitOfMeasure!]?.[nutrientFactInBaseUnits.nutrientName];
-      if (nutrientAmount !== undefined) {
-        nutrientsAtCheapestCost[nutrientFactInBaseUnits.nutrientName] ||= {
-          nutrientName: nutrientFactInBaseUnits.nutrientName,
-          quantityAmount: {
-            uomAmount: 0,
-            uomName: nutrientUnitOfMeasure!,
-            uomType: "mass"
-          },
-          quantityPer: {
-            uomAmount: 100,
-            uomName: nutrientUnitOfMeasure!,
-            uomType: "mass"
+  
+      let nutrientFactInBaseUnits: NutrientFact | null = null;
+      let nutrientUnitOfMeasure: string | null = null;
+  
+      for (const product of products) {
+        for (const supplierProduct of product.supplierProducts) {
+          const costPerBaseUnit = GetCostPerBaseUnit(supplierProduct);
+          if (costPerBaseUnit === null) {
+            console.log(`No cost per base unit found for product: ${product.productName}`);
+            continue;
           }
-        };
-        nutrientsAtCheapestCost[nutrientFactInBaseUnits.nutrientName].quantityAmount.uomAmount += nutrientAmount.uomAmount;
+  
+          cheapestCost = Math.min(cheapestCost, costPerBaseUnit);
+        }
+  
+        const nutrientFact = product.nutrientFacts
+          .map(nf => GetNutrientFactInBaseUnits(nf))
+          .find(nf => nf !== null);
+        if (nutrientFact === null) {
+          console.log(`No nutrient information found for product: ${product.productName}`);
+          continue;
+        }
+        if (nutrientFact !== undefined) {
+            nutrientFactInBaseUnits = nutrientFact;
+        }
+        nutrientUnitOfMeasure = lineItem.unitOfMeasure.toString();
+        const nutrientAmount = nutrientFactInBaseUnits?.[nutrientUnitOfMeasure!]?.[nutrientFactInBaseUnits.nutrientName];
+        if (nutrientAmount !== undefined && nutrientFactInBaseUnits !== null && nutrientFactInBaseUnits !== undefined) {
+          nutrientsAtCheapestCost[nutrientFactInBaseUnits.nutrientName] ||= {
+            nutrientName: nutrientFactInBaseUnits.nutrientName,
+            quantityAmount: {
+              uomAmount: 0,
+              uomName: nutrientUnitOfMeasure!,
+              uomType: "mass"
+            },
+            quantityPer: {
+              uomAmount: 100,
+              uomName: nutrientUnitOfMeasure!,
+              uomType: "mass"
+            }
+          };
+            nutrientsAtCheapestCost[nutrientFactInBaseUnits.nutrientName].quantityAmount.uomAmount += nutrientAmount.uomAmount;
       }
     }
   }
